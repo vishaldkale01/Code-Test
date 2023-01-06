165969ce4fbe50c49d0a8d4afd192eb35eced0cc
vishaldkale01 <90303175+vishaldkale01@users.noreply.github.com>
2023-01-06 11:36:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Code-Test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9uwvvf)
---
+++ index.js
@@ -0,0 +1,12 @@
+const _ = require('lodash');
+let model = {
+  xyz: 'vishal',
+  c: 'c',
+  new: '',
+};
+let arr = ['vishal', 'kale', 'dfdfsf'];
+let res = (model.nee = _.each(arr, (val) => {
+  return model.val;
+}));
+console.log(model);
+console.log('rs,', res);
